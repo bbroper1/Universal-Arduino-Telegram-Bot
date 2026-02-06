8a96571185b893f518175a521eb40e07d7c2c6c8
bbroper1 <bbroper1@gmail.com>
2026-02-05 21:52:58 -0600

    Refactor connection logic to apply certificate

--- src/UniversalTelegramBot.cpp
+++ src/UniversalTelegramBot.cpp
@@ -72,11 +72,13 @@ String UniversalTelegramBot::sendGetToTelegram(const String& command) {
   String body, headers;
   
   // Connect with api.telegram.org if not already connected
-  if (!client->connected()) {
-    #ifdef TELEGRAM_DEBUG  
-        Serial.println(F("[BOT]Connecting to server"));
-    #endif
-    if (!client->connect(TELEGRAM_HOST, TELEGRAM_SSL_PORT)) {
+if (!client->connected()) {
+  applyCertificate();
+
+  #ifdef TELEGRAM_DEBUG  
+      Serial.println(F("[BOT]Connecting to server"));
+  #endif
+  if (!client->connect(TELEGRAM_HOST, TELEGRAM_SSL_PORT)) {
       #ifdef TELEGRAM_DEBUG  
         Serial.println(F("[BOT]Connection error"));
       #endif
